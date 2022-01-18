5f409d6bdcf7fab3194f7efd78b74181c0f8e6bc
Utsav-Benchmark-Git <utsavsalokhe@benchmarkit.solutions>
2022-01-18 17:27:17 +0530

    Assignment on Action Result Methods

---
+++ Udemy_Assignment_ActionResult/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Udemy_Assignment_ActionResult.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index(string arg)
+        {
+            if(arg == "sample")
+            {
+                return File("~/sample.pdf", "application/pdf");
+            }
+            else if(arg == "about")
+            {
+                return RedirectToAction("About");
+            }
+            else if (arg == "login")
+            {
+                return View("Login");
+            }
+            else
+            {
+                return Content("You entered : "+arg);
+            }
+        }
+        public ActionResult About()
+        {
+            return Content("ABout Page");
+        }
+    }
+}
---
+++ Udemy_Assignment_ActionResult/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Udemy_Assignment_ActionResult.MvcApplication" Language="C#" %>
