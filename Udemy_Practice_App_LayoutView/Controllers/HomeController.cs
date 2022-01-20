45113592c5efe15cef418fc5564b6849a939a9ae
Utsav-Benchmark-Git <utsavsalokhe@benchmarkit.solutions>
2022-01-20 18:09:59 +0530

    Assignment 20th Jan on Views

---
+++ Udemy_Practice_App_LayoutView/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Udemy_Practice_App_LayoutView.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+        public ActionResult About()
+        {
+
+            return View();
+        }
+        public ActionResult Contact()
+        {
+
+            return View();
+        }
+    }
+}
---
+++ Udemy_Practice_App_LayoutView/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Udemy_Practice_App_LayoutView.MvcApplication" Language="C#" %>
